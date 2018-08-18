d28410fcdd51e1f57cdca7d663e9bb28fc3b0714
Ian Izaguirre <ian@izaguir.re>
2018-08-18 19:56:27 -0400

    Add: Fizz Buzz

--- Basics/Comparison-Challenge/fizz-buzz.py
+++ Basics/Comparison-Challenge/fizz-buzz.py
@@ -1,3 +1,26 @@
 name = input("Please enter your name: ")
 number = int(input("Please enter a number: "))
 
+by_three = (number % 3 == 0)
+by_five = (number % 5 == 0)
+by_both = (by_three and by_five)
+
+grab_name = print("Hey {}!".format(name))
+grab_number = print("The number {}...".format(number))
+
+if by_both:
+  deduce_number_type = print("is a FizzBuzz number.")
+
+elif by_three:
+  deduce_number_type = print("is a Fizz number.")
+
+elif by_five:
+  deduce_number_type = print("is a Buzz number")
+
+else:
+  deduce_number_type = print("is nether a fizzy or a buzzy number.")
+
+# My Program
+grab_name
+grab_number
+deduce_number_type
