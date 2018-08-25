bb9cc0b1d00b9127ae259173976e1b42f509849d
Ian Izaguirre <ian@izaguir.re>
2018-08-24 20:03:38 -0400

    Add: functions practice

---
+++ Basics/Functions/Examples.py
@@ -0,0 +1,14 @@
+# Example 1
+def yell(text):
+  text = text.upper()
+  number_of_characters = len(text)
+  result = text + "!" * (number_of_characters // 2)
+  print(result)
+
+# Example 2 / "Split the Bill"
+def split_check(total, number_of_people):
+  cost_per_person = total / number_of_people
+  return cost_per_person
+
+amount_due = split_check(84.97, 4)
+print("Each person owes ${}".format(amount_due))
