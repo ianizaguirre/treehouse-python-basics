52eaa71fc607d3abacf818156d9127144bf73928
Ian Izaguirre <ian@izaguir.re>
2018-08-24 20:09:35 -0400

    Add: import module for math

--- Basics/Functions/Examples.py
+++ Basics/Functions/Examples.py
@@ -5,10 +5,14 @@ def yell(text):
   result = text + "!" * (number_of_characters // 2)
   print(result)
 
+
 # Example 2 / "Split the Bill"
+import math
+
 def split_check(total, number_of_people):
-  cost_per_person = total / number_of_people
+  cost_per_person = math.ceil(total / number_of_people)
   return cost_per_person
 
 amount_due = split_check(84.97, 4)
+
 print("Each person owes ${}".format(amount_due))
